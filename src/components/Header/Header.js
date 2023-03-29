b5bf7bd3808ca20956e70b94db7f295e6fb3e932
TamaraBogantseva <tamara.bogantseva@compose.us>
2023-03-29 11:05:29 +0200

    chore: add header styles
    Signed-off-by: TamaraBogantseva <tamara.bogantseva@compose.us>

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import styled from 'styled-components/macro';
+import React from "react";
+import styled from "styled-components/macro";
 
-import { COLORS, WEIGHTS } from '../../constants';
-import Logo from '../Logo';
-import SuperHeader from '../SuperHeader';
+import { COLORS, WEIGHTS } from "../../constants";
+import Logo from "../Logo";
+import SuperHeader from "../SuperHeader";
 
 const Header = () => {
   // Our site features two visual headers, but they should be
@@ -29,9 +29,16 @@ const Header = () => {
 const MainHeader = styled.div`
   padding: 0 32px;
   border-bottom: 1px solid ${COLORS.gray[300]};
+  display: flex;
+  min-height: 72px;
+  align-items: center;
 `;
 
-const Nav = styled.nav``;
+const Nav = styled.nav`
+  display: flex;
+  gap: 48px;
+  flex: 3;
+`;
 
 const NavLink = styled.a`
   font-size: 1.125rem;
@@ -42,6 +49,9 @@ const NavLink = styled.a`
 
   &:first-of-type {
     color: ${COLORS.secondary};
+  }
+  &:hover {
+    color: ${COLORS.primary};
   }
 `;
 
